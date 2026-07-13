1b96ebd4f217ad53329ca38026ec3360bde381b2
Aryan Chandna <aryan.chandna@icloud.com>
2026-07-13 16:31:44 -0700

    fix: add loading attribute to iframe for improved performance

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -49,6 +49,7 @@ export default function Skills() {
                   frameBorder="0" 
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                   allowFullScreen
+                  loading="lazy"
                 ></iframe>
               </div>
             )}
